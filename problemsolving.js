e5847d39b2533d2baba59311d6f5b8f4d0689fa9
farihamehereen15 <mitifariha7@gmail.com>
2022-03-25 04:08:19 +0600

    first commit

---
+++ problemsolving.js
@@ -0,0 +1,159 @@
+// // Solve the problem with Javascript  to find the maximum between two numbers.
+// var x = 10;
+// var y = 20; 
+
+
+
+// if ( x > y)
+// {
+//     console.log(" x is biggest");
+// } else {
+//     console.log(" y is biggest");
+// }
+
+// // Solve the problem with Javascript  to find a maximum between three numbers.
+// var x = 10;
+// var y = 20;
+// var z = 30;
+
+// if (x >= y && x >= z)
+// {
+//     console.log(" x is biggest = ",x);
+// }
+
+// if (y >= x && y >= z)
+// {
+//     console.log(" y is biggest = ",y);
+// }
+
+
+
+// if (z >= x && z >= y)
+// {
+//     console.log(" z is biggest = ",z);
+// }
+
+
+// // Solve the problem with Javascript  to check whether a number is negative, positive or zero.
+// var number = -5;
+
+// if (number > 0) {
+//     console.log("The number is positive");
+// }
+
+
+// else if (number == 0) {
+//   console.log("The number is zero");
+// }
+
+
+// else {
+//      console.log("The number is negative");
+// }
+
+
+// // Solve the problem with Javascript  to check whether a number is divisible by 40 or not.
+
+// var number = 80;
+// if (number % 40 == 0)
+// {
+//     console.log(" Number is divisible by 40");
+// }
+// else
+// {
+//     console.log(" Number is not divisible by 40");
+// }
+
+
+// // Solve the problem with Javascript  to check whether a number is even or odd.
+
+// var number = 2;
+// if (number % 2 == 0)
+// {
+//   console.log("even");
+// }
+// else
+// {
+//   console.log("odd");
+// }
+
+
+// // Solve the problem with Javascript  to check whether a character is in the alphabet or not.
+// var ch = 'rgb';
+
+//  if( (ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))
+//   {
+//     console.log("The entered character is an Alphabet");
+//   }
+
+// else
+//   {
+//     console.log("The entered character is not an Alphabet");
+//   }
+
+
+// // Solve the problem with Javascript  to check weather the temperature is hot, cold or normal
+// var Temperature = "cold";
+
+// if (Temperature == "hot")
+
+// {
+//     console.log("Temperature is hot.");
+// }
+
+
+// else if (Temperature == "cold")
+
+// {
+//     console.log("Temperature is cold.");
+// }
+
+// else{
+//     console.log("Temperature is normal.");
+// }
+
+
+
+// // Given 3 subjects Math, English and Physics find the total marks and average of 3 subjects.
+// // Find out if the average grade:
+// // If Percentage >= 90% then print “Grade A”
+// // Percentage >= 80% : Grade B
+// // Percentage >= 70% : Grade C
+// // Percentage >= 60% : Grade D
+// // Percentage >= 40% : Grade E
+// // Percentage < 40% : Grade F
+
+// var math = 90;
+// var english = 70;
+// var physics = 80;
+// var total ;
+// var average;
+
+// total = math + english + physics;
+// average = total / 3;
+// console.log(total);
+// console.log(average);
+
+// if(average >= 90){
+//     console.log("Grade A");
+// }
+// else if (average >= 80)
+// {
+//     console.log("Grade B");
+// }
+// else if (average >= 70)
+// {
+//     console.log("Grade C");
+// }
+// else if (average >= 60)
+// {
+//     console.log("Grade D");
+// }
+// else if (average >= 40)
+// {
+//     console.log("Grade E");
+// }
+// else if (average < 40)
+// {
+//     console.log("Grade F");
+// }
